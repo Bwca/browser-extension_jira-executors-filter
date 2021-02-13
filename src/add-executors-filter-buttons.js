e67f91b7f6ae5fce654d01918300968f5c5e12fb
Volodymyr Yepishev <i.m.bwca@gmail.com>
2021-02-13 16:00:14 +0300

    added updates on task reloads

--- src/add-executors-filter-buttons.js
+++ src/add-executors-filter-buttons.js
@@ -1,8 +1,8 @@
-function main() {
+function getExecutionersListFromAvatarsInIssues() {
     const avatars = Array.from(document.querySelectorAll(".ghx-avatar-img"));
 
     if (!avatars || !avatars.length) {
-        throw "WTF DUDE?";
+        return;
     }
 
     const executorNames = new Set(avatars.map((i) => i.dataset.tooltip));
@@ -14,23 +14,22 @@ function main() {
             avatar: el.src,
         };
     });
-    createIsCurrentlyActiveClass();
-    addExecutorIcons(executors);
+    appendExecutorButtonsToFilterContainer(executors);
 }
 
-function getAllIssues() {
+function getAllIssueElements() {
     return Array.from(
         document.querySelectorAll(".js-issue,.ghx-parent-group.ghx-away.js-fake-parent")
     );
 }
 
-function showAllIssues() {
-    const issues = getAllIssues();
+function setAllIssuesVisible() {
+    const issues = getAllIssueElements();
     issues.forEach((i) => (i.style.display = ""));
 }
 
-function filterByExecutor(executor) {
-    const issues = getAllIssues();
+function setIssuesVisibleByExecutor(executor) {
+    const issues = getAllIssueElements();
 
     issues.forEach((i) => {
         const isSelectedExecutorsIssue = i.querySelector(
@@ -45,32 +44,33 @@ function filterByExecutor(executor) {
     });
 }
 
-function createIsCurrentlyActiveClass() {
+function createIsCurrentlyActiveExecutorClassStyle() {
     const style = document.createElement("style");
     style.innerHTML =
         ".executor-filter.is-currently-selected { border: 5px solid limegreen !important; } .executor-filter {border: 5px solid white !important; cursor: pointer}";
     document.getElementsByTagName("head")[0].appendChild(style);
 }
 
-function addExecutorIcons(executors) {
+function appendExecutorButtonsToFilterContainer(executors) {
     const filtersContainer = document.getElementById("js-work-quickfilters");
     executors.forEach((i) => {
         const dd = document.createElement("dd");
         const img = document.createElement("img");
         img.src = i.avatar;
+        img.id = i.avatar;
         img.classList.add("ghx-avatar-img");
         img.classList.add("executor-filter");
         img.title = i.executor.replace(/^Исполнитель: /, "");
         img.onclick = () => {
             const isCurrenylySelected = img.classList.contains("is-currently-selected");
-            const executorFilters = getExecutorsFilterButtons();
+            const executorFilters = getExecutionersFilterButtonsAsArray();
 
             executorFilters.forEach((i) => i.classList.remove("is-currently-selected"));
 
             if (isCurrenylySelected) {
-                showAllIssues();
+                setAllIssuesVisible();
             } else {
-                filterByExecutor(i);
+                setIssuesVisibleByExecutor(i);
                 img.classList.add("is-currently-selected");
             }
         };
@@ -79,8 +79,48 @@ function addExecutorIcons(executors) {
     });
 }
 
-function getExecutorsFilterButtons() {
+function getExecutionersFilterButtonsAsArray() {
     return Array.from(document.querySelectorAll("img.executor-filter"));
 }
 
-setTimeout(main, 1000);
+function removeExecutionersFilterButtonsFromFiltersContainer() {
+    const filtersContainer = document.getElementById("js-work-quickfilters");
+    Array.from(filtersContainer.querySelectorAll("dd"))
+        .filter((i) => !!i.querySelector("img.executor-filter"))
+        .forEach((i) => i.remove());
+}
+
+function updateExecutorFilterButtonsList() {
+    const currentlySelectedExecutor = document.querySelector(
+        ".ghx-avatar-img.executor-filter.is-currently-selected"
+    );
+    removeExecutionersFilterButtonsFromFiltersContainer();
+    getExecutionersListFromAvatarsInIssues();
+    if (currentlySelectedExecutor) {
+        const updatedCurrentExecutorIcon = document.getElementById(
+            currentlySelectedExecutor.id
+        );
+        updatedCurrentExecutorIcon && updatedCurrentExecutorIcon.click();
+    }
+}
+
+(function main() {
+    createIsCurrentlyActiveExecutorClassStyle();
+
+    const targetNode = document.querySelector("div.ghx-throbber");
+
+    const config = { attributes: false, childList: true, subtree: true };
+
+    const callback = (mutationsList) =>
+        mutationsList.forEach((mutation) => {
+            const hasTasksListUpdated =
+                mutation.type === "childList" && !!mutation.removedNodes.length;
+            if (hasTasksListUpdated) {
+                updateExecutorFilterButtonsList();
+            }
+        });
+
+    const observer = new MutationObserver(callback);
+
+    observer.observe(targetNode, config);
+})();
